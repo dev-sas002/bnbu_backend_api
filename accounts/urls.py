70c0ca03fcb1209a5a77b57f67914bb9ec4ed178
Ahsan Ellahi <mail.ahsan.ellahi@gmail.com>
2024-10-30 16:11:22 +0500

    feat: implement one-to-many relationship - update models, views, serializers, and URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from .forms import EmailAuthenticationForm
 from .views import (
     UserListCreateView,
-    UserDetailView,  # Add this line
+    UserDetailView,
     FirstTimePasswordUpdateView,
     password_change,
     password_reset,
@@ -12,12 +12,25 @@ from .views import (
     LoginView, 
     LogoutView,
     user_profile,
+    ClientListCreateView,
+    ClientDetailView,
+    CustomerListView,
+    CustomerCreateView,
+    CustomerDetailView,
 )
 
 urlpatterns = [
     path('users/', UserListCreateView.as_view(), name='user-list-create'),  # For admin to view and create users
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),  # For admin to update and delete users
     path('users/update-password/<int:pk>/', FirstTimePasswordUpdateView.as_view(), name='update-password'),  # For first-time password updates
+    
+    path('clients/', ClientListCreateView.as_view(), name='client-list-create'),  # List and create clients
+    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client-detail'),  # Get, update, delete a client
+    path('clients/<int:client_id>/customers/', CustomerListView.as_view(), name='customer-list'),  # List customers for a specific client
+    path('customers/', CustomerListView.as_view(), name='all-customer-list'),  # List all customers for admin
+    path('clients/<int:client_id>/customers/create/', CustomerCreateView.as_view(), name='customer-create'),  # Create a customer linked to a client
+    path('clients/<int:client_id>/customers/<int:pk>/', CustomerDetailView.as_view(), name='customer-detail'),  # Get, update, delete a customer
+
     path('password/change/', password_change, name='password-change'),  # Endpoint for changing the password
     path('password/reset/', password_reset, name='password-reset'),  # Endpoint for requesting password reset
     path('dashboard/', dashboard, name='dashboard'),  # User dashboard
