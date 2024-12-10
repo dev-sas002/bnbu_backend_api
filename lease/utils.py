6e578d2cbb850f285c22a828b43ad1a5d805599e
Ahsan Ellahi <mail.ahsan.ellahi@gmail.com>
2024-12-10 20:52:37 +0500

    refractor

--- lease/utils.py
+++ lease/utils.py
@@ -4,7 +4,6 @@ import json
 import logging
 from datetime import datetime, timezone
 import os
-from pathlib import Path
 import re
 import time
 import PyPDF2
@@ -76,11 +75,8 @@ def analyze_document_with_gpt(document):
     logger.info(f"Analyzing document ID: {document.id}")
     try:
         # Construct the file path using BASE_DIR
-        file_path = Path(settings.BASE_DIR, document.file.path)
-
-        print(
-            f"I am in analyze_document_with_gpt function and the Document file path is: {file_path}, {settings.BASE_DIR}"
-        )
+        file_path = os.path.join(settings.BASE_DIR , document.file.path)
+        print(f"I am in analyze_document_with_gpt function and the Document file path is: {file_path}, {settings.BASE_DIR}")
 
         if not os.path.exists(file_path):
             logger.error(
